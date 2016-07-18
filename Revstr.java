a96183ff4ca615320702e6e096632dc21bd0df7a
sudarsananpadmanaban <sudar1127@gmail.com>
2016-07-18 14:16:28 +0530

    Add files via upload

---
+++ Revstr.java
@@ -0,0 +1,22 @@
+
+package revstr;
+
+import java.io.*;
+import java.io.*;
+
+
+public class Revstr {
+    public static void main(String[] args) {
+        String input="";
+        System.out.println("Enter the input string");
+        try
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            input = br.readLine();
+            char[] try1= input.toCharArray();
+            for (int i=try1.length-1;i>=0;i--)
+            System.out.print(try1[i]);
+        }
+        catch (IOException e) {
+        }
+}}
